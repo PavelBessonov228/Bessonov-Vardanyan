50221c8a8ea11c1fb4a0210f9205b44fa9c954ab
Pavel Bessonov <89220292577a@gmail.com>
2024-12-11 23:47:21 +0300

    отправил запрос и проверил его статус

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 # URL страницы с Википедии
 url = "https://en.wikipedia.org/wiki/ISO_4217"
+
+# Отправка GET-запроса
+response = requests.get(url)
+
+# Проверка статуса запроса
+if response.status_code == 200:
