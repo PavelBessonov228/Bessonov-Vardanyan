d94fb8e77d8797b15e0c752e4ca63e4fff3c629d
Pavel Bessonov <89220292577a@gmail.com>
2024-12-20 12:56:05 +0300

    добавил ф-ию для получения хтмл-кода с википедии

--- lalalala.py
+++ lalalala.py
@@ -2,3 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def fetch_data_from_wikipedia(url):
+    """
+    Функция для получения HTML-кода страницы с Википедии.
+    :param url: URL страницы Википедии.
+    :return: объект BeautifulSoup.
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        return BeautifulSoup(response.text, 'html.parser')
+    else:
+        raise Exception(f"Ошибка запроса: {response.status_code}")
+
